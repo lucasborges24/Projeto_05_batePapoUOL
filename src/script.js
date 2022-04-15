208d27f4668c954a457415a517266e286e60c87c
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-04-14 22:47:18 -0300

    implementation: receive messages from the server; update messages from the server; send messages to the server.

--- src/script.js
+++ src/script.js
@@ -34,8 +34,73 @@ function messages() {
     promise.then(putMessages)
 }
 
+function refreshMessage() {
+    const refresh = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    refresh.then(putMessages)
+}
+
 function putMessages(message) {
-    console.log(message.data[5].from)
+    let messages = message.data
+    let main = document.querySelector("main");
+    main.innerHTML = "";
+
+    for (let i = 89; i < messages.length; i ++) {
+        main.innerHTML += divMessage(messages[i])
+    }
+    setInterval(refreshMessage, 3000)
+}
+
+function divMessage(mensagem) {
+    if (mensagem.type === "status") {
+        return `
+        <div class="messages ${mensagem.type}">
+            <div class="time">
+                <h6>(${mensagem.time})</h6>
+            </div>
+            <div class="recipient">
+                <h6><span>${mensagem.from}</span></h6>
+            </div>
+            <div class="status">
+                <h6>
+                    ${mensagem.text}
+                </h6>
+            </div>
+        </div>
+        `
+    } else if (mensagem.type === "message") {
+        return `
+        <div class="messages ${mensagem.type}">
+            <div class="time">
+                <h6>(${mensagem.time})</h6>
+            </div>
+            <div class="recipient">
+                <h6><span>${mensagem.from}</span> para <span>${mensagem.to}:</span></h6>
+            </div>
+            <div class="texto">
+                <h6>
+                    ${mensagem.text}
+                </h6>
+            </div>
+        </div>
+        `
+    } else {
+        return `
+        <div class="messages ${mensagem.type}">
+            <div class="time">
+                <h6>(${mensagem.time})</h6>
+            </div>
+            <div class="recipient">
+                <h6><span>${mensagem.from}</span> reservadamente para <span>${mensagem.to}:</span></h6>
+            </div>
+            <div class="texto">
+                <h6>
+                    ${mensagem.text}
+                </h6>
+            </div>
+        </div>
+        `
+    }
+    
 }
 
 function treatError(error) {
@@ -44,4 +109,27 @@ function treatError(error) {
     if (invalideUser) {
         sendUserName();
     }
+}
+
+function sendMessage() {
+    const objMessage = {
+        from: UserNameReq.name,
+        to: "Todos",
+        text: document.querySelector("input").value,
+        type: "message",
+    }
+
+    const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", objMessage);
+    console.log(requisicao.then)
+
+    requisicao.then(sendSucess)
+    requisicao.catch(sendError)
+}
+
+function sendSucess() {
+    document.querySelector("input").value = ""
+}
+
+function sendError() {
+    window.location.reload()
 }
