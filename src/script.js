94a0a9fa67a83a7d19b95c976167d86fc8ef2c59
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-04-18 17:31:56 -0300

    add final comments. The final project is done

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
+// GLOBAL VARIABLES
 let UserNameReq
 let contacts = [];
 let checkContact = ["", ""];
 
-
-
-
+// ENABLE ENTER KEY FOR INPUTS
 enterKeyboardMessage();
 
 
@@ -22,26 +21,26 @@ function sendUserName() {
     userName();
     const requisicao = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', UserNameReq)
     intervals();
-    document.querySelector(".loader").classList.remove("none");
-    document.querySelector(".user-login").classList.add("none");
+    document.querySelector(".loader").classList.remove("none"); // DISPLAY LOAD 
+    document.querySelector(".user-login").classList.add("none"); // CUT LOGIN SCREEN
     requisicao.then(messages);
     requisicao.catch(leadUserNameError)
-}
-
-
-function loginPage() {
-    document.querySelector(".loader").classList.add("none");
-    document.querySelector(".img-login").classList.add("none");
-    document.querySelector("header").classList.remove("none")
-    document.querySelector("footer").classList.remove("none")
-    document.querySelector("main").classList.remove("none")
 }
 
 // Functions executed periodically 
 function intervals() {
     setInterval(connectionStable, 4000) // keep connection with the server
     setInterval(refreshMessage, 3000) // refresh messages after 3s
-    setInterval(refreshContacts, 10000)
+    setInterval(refreshContacts, 10000) // refresh contacts after 10s
+}
+
+// active/desactive display in the login -> message transition
+function loginPage() {
+    document.querySelector(".loader").classList.add("none");
+    document.querySelector(".img-login").classList.add("none");
+    document.querySelector("header").classList.remove("none")
+    document.querySelector("footer").classList.remove("none")
+    document.querySelector("main").classList.remove("none")
 }
 
 // function to keep connection with the server
@@ -55,17 +54,20 @@ function Unlog() {
     window.location.reload() // if the UserName isn't send to server, the page are reloaded
 }
 
+// replace the messages
 function refreshMessage() {
     const refresh = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     refresh.then(putMessages)
 }
 
+// get the messages from the server
 function messages() {
     loginPage()
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(putMessages);
 }
 
+// put messages in the screen
 function putMessages(message) {
     let messages = message.data
     let main = document.querySelector("main");
@@ -79,6 +81,7 @@ function putMessages(message) {
     scrolll();
 }
 
+// html message content from each message case
 function divMessage(mensagem) {
     if (mensagem.type === "status") {
         return `
@@ -119,11 +122,13 @@ function divMessage(mensagem) {
     
 }
 
+// function to scroll to last message smoothly
 function scrolll() {
     const lastMessage = document.querySelector("main .messages:last-child")
     lastMessage.scrollIntoView({behavior: "smooth"});
 }
 
+// lead with twofold name
 function leadUserNameError(error) {
     const invalideUser = (error.response.status === 400)
     let valideUser = (error.response.status === 200);
@@ -133,6 +138,7 @@ function leadUserNameError(error) {
     }
 }
 
+// send message to the server
 function sendMessage() {
     let objMessage;
     if (checkContact[1] === "Reservadamente") {
@@ -158,6 +164,7 @@ function sendMessage() {
     requisicao.catch(sendError)
 }
 
+// enter key function
 function enterKeyboardMessage() {
     const input = document.querySelector(".footer-input") 
     input.addEventListener('keydown', function(event) {
@@ -174,14 +181,17 @@ function enterKeyboardMessage() {
     })
 }
 
+// clean the message input
 function sendSucess() {
     document.querySelector(".footer-input input").value = ""
 }
 
+// reload the screen if have error
 function sendError() {
     window.location.reload()
 }
 
+// open sidebar and get the participants
 function sidebar() {
     const shadow = document.querySelector(".shadow").classList.remove("none")
     const sidebar = document.querySelector(".sidebar").classList.remove("none")
@@ -189,6 +199,7 @@ function sidebar() {
     promise.then(sidebarContact)
 }
 
+// push participants in the contacts array
 function sidebarContact(contact) {
     contacts = [];
     for (let i = 0; i < contact.data.length; i++) {
@@ -197,6 +208,7 @@ function sidebarContact(contact) {
     writeContact()
 }
 
+// write participants in the screen
 function writeContact() {
     const contatos = document.querySelector(".contacts");
     contatos.innerHTML = `<div class="contact" onclick="check(this)">
@@ -223,6 +235,7 @@ function writeContact() {
     }
 }
 
+// put check icon in the selected participant
 function check(element) {
     const contatozinho = document.querySelectorAll(".contact")
     const checkzinho = document.querySelectorAll(".check-contact")
@@ -237,9 +250,9 @@ function check(element) {
     checkContact[0] = element.childNodes[1].childNodes[3].innerHTML
 
     inputText();
-    
 }
 
+// put check icon in the selected visibility
 function visibility(element) {
     const visibilidadezinha = document.querySelectorAll(".visibility-type")
     const checkzinho = document.querySelectorAll(".check-visibility")
@@ -255,18 +268,20 @@ function visibility(element) {
     inputText();
 }
 
+// text below input showing the recipient's name and the visibility
 function inputText() {
     const inputtext = document.querySelector("footer p")
     inputtext.innerHTML = `Enviando para ${checkContact[0]} (${checkContact[1]})`
 }
 
+// refresh contacts after 10s
 function refreshContacts() {
     console.log("deu certo")
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
     promise.then(sidebarContact)
 }
 
-
+// return to messages when shadow screen are clicked
 function backMessages() {
     const shadow = document.querySelector(".shadow").classList.add("none")
     const sidebar = document.querySelector(".sidebar").classList.add("none")
